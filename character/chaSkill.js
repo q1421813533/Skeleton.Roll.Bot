8a7aba279291d879700d660345ab6d12e3a16d6a
q1421813533 <113162584+q1421813533@users.noreply.github.com>
2024-12-04 22:57:22 +0800

    Create chaSkill.js

---
+++ character/chaSkill.js
@@ -0,0 +1,10 @@
+class chaSkill{
+    constructor(){
+        this.name="";
+        this.value=0;
+        this.sound=function(){
+            console.log("喵喵喵喵喵喵喵喵");
+        }
+    }
+}
+export default chaSkill;
