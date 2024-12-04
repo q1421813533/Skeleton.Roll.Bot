0c9327b8aa3e1514a23ac841fd4f8837c8c9143d
q1421813533 <113162584+q1421813533@users.noreply.github.com>
2024-12-05 00:34:31 +0800

    Delete character/chaSkill.js

--- character/chaSkill.js
+++
@@ -1,9 +0,0 @@
-class chaSkill{
-    constructor(skillName,skillCode,skillValue,skillType){
-        this.name=skillName;
-        this.code=skillCode;
-        this.value=skillValue;
-        this.type=skillType;
-    }
-}
-module.exports = chaSkill;
